ccd89936fe12317156af2d8cba6d071568f2b926
unknown <huynamvn93@gmail.com>
2021-06-09 16:45:56 +0700

    bangtinh

---
+++ caculator.js
@@ -0,0 +1,20 @@
+function cong(){
+    let  a= +document.getElementById("lan1").value;
+    let  b= +document.getElementById("lan2").value;
+    let c = (a+b); alert(c);
+}
+function tru(){
+    let  a= +document.getElementById("lan1").value;
+    let  b= +document.getElementById("lan2").value;
+    let c = (a-b); alert(c);
+}
+function nhan(){
+    let  a= +document.getElementById("lan1").value;
+    let  b= +document.getElementById("lan2").value;
+    let c = (a*b); alert(c);
+}
+function chia(){
+    let  a= +document.getElementById("lan1").value;
+    let  b= +document.getElementById("lan2").value;
+    let c = (a/b); alert(c);
+}
